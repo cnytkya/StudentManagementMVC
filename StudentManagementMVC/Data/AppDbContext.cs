8d0b03434c303acc6391934bf12434214d3e93b3
kayac <kayac@CUNEYT>
2025-06-14 21:48:48 +0300

    Add project files.

---
+++ StudentManagementMVC/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentManagementMVC.Models;
+
+namespace StudentManagementMVC.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; } 
+    }
+}
---
+++ StudentManagementMVC/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagementMVC.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }//Benzersiz(uniq) bir kimlik oluşturur.
+        //public string? Name { get; set; }Eğer null yani boş geçilebilir olsun istiyorsak soru(?) işareti koymamız yeterli olacaktır.
+        [Required]//Name alanı zorunludur. Kullanıcı burayı boş geçemez
+        public string Name { get; set; }
+        [Range(18,45)]
+        public int Age { get; set; }
+        //[Required]
+        public string? Department { get; set; }
+    }
+}
