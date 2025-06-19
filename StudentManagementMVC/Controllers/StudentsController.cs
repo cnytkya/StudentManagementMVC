52d17355fd4ebeb36fb9c26ae2e58a1c562a8fe4
kayac <kayac@CUNEYT>
2025-06-19 22:15:52 +0300

    Implement student list with database integration
    Updated `StudentsController` to use dependency injection for `AppDbContext` and retrieve students asynchronously in the `Index` method. Modified `Index.cshtml` to display a list of students with relevant details and action buttons, enhancing the user interface for managing students.

--- StudentManagementMVC/Controllers/StudentsController.cs
+++ StudentManagementMVC/Controllers/StudentsController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentManagementMVC.Data;
 
 namespace StudentManagementMVC.Controllers
 {
     public class StudentsController : Controller
     {
-        public IActionResult Index()
+        //DI ile veritabanı entegrasyonunu çağırıyoruz.
+        private readonly AppDbContext _appDbContext;
+
+        //Constructor metot(yapıcı metot)
+        public StudentsController(AppDbContext appDbContext)
         {
-            return View();
+            _appDbContext = appDbContext;
         }
+
+        public async Task<IActionResult> Index()
+        {
+            var listAllStudents = await _appDbContext.Students.ToListAsync();
+            return View(listAllStudents);
+            //return View(await _appDbContext.Students.ToListAsync());
+        }
+
     }
 }
