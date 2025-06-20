bb311d19c20b4638f4ff263c389096dd557f8661
kayac <kayac@CUNEYT>
2025-06-20 22:29:40 +0300

    Add create and edit functionality for student records
    Implement Create and Edit methods in StudentsController.cs
    to handle student record management with model state validation.
    Enhance Create.cshtml and Edit.cshtml with new form layouts,
    validation messages, and client-side validation for improved
    user experience. Adjust age validation in Edit.cshtml to
    allow ages between 18 and 45.

--- StudentManagementMVC/Controllers/StudentsController.cs
+++ StudentManagementMVC/Controllers/StudentsController.cs
@@ -48,6 +48,40 @@ namespace StudentManagementMVC.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(Student student)
         {
+            if (ModelState.IsValid)
+            {
+                _appDbContext.Students.Add(student);
+                //kullanıcıdan gelen değeri kaydederken savchange uygula
+                await _appDbContext.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var student = await _appDbContext.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _appDbContext.Update(student);
+                await _appDbContext.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
             return View(student);
         }
     }
