d8330f12b4edc7cf3f48485eb39d949d3070f93f
kayac <kayac@CUNEYT>
2025-06-20 20:33:47 +0300

    snapfilo kaldırıldı

--- StudentManagementMVC/Controllers/StudentsController.cs
+++ StudentManagementMVC/Controllers/StudentsController.cs
@@ -7,16 +7,17 @@ namespace StudentManagementMVC.Controllers
     public class StudentsController : Controller
     {
         //DI ile veritabanı entegrasyonunu çağırıyoruz.
-        private readonly AppDbContext _appDbContext;
+        private readonly AppDbContext _appDbContext;// Veritabanı bağlamını (DbContext) tutan özel ve sadece okunabilir bir alan tanımlar.
 
         //Constructor metot(yapıcı metot)
-        public StudentsController(AppDbContext appDbContext)
+        public StudentsController(AppDbContext appDbContext)// StudentsController'ın yapıcı (constructor) metodu.
         {
-            _appDbContext = appDbContext;
+            _appDbContext = appDbContext;// Dependency Injection ile sağlanan AppDbContext örneğini _context alanına atar.
         }
 
         public async Task<IActionResult> Index()
         {
+            // _context.Students.ToListAsync() ile veritabanındaki tüm öğrenci kayıtlarını asenkron olarak çeker.
             var listAllStudents = await _appDbContext.Students.ToListAsync();
             return View(listAllStudents);
             //return View(await _appDbContext.Students.ToListAsync());
